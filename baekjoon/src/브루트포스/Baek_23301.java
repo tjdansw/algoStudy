1b0499b632fc956a1ed5771e6949914dd3a6ab7e
SeongMun Woo <tjdans7665@gmail.com>
2024-09-19 08:37:57 +0900

    add default setting

---
+++ baekjoon/src/브루트포스/Baek_23301.java
@@ -0,0 +1,20 @@
+package 브루트포스;
+
+import java.util.*;
+import java.io.*;
+
+public class Baek_23301 {
+	static int n,t,k;
+	static int[][] times;
+	public static void main(String[] args) throws Exception{
+		System.setIn(new FileInputStream("src/input/Baek_23301.txt"));
+		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		
+		String[] line = br.readLine().trim().split(" ");
+		n = Integer.parseInt(line[0]);
+		t = Integer.parseInt(line[0]);
+		for(int p=0;p<n;p++) {
+			 k = Integer.parseInt(br.readLine());
+		}
+	}
+}
