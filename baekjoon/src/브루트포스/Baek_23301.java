b9093eb4f0fcaa03fdf2872087d7a4554a2004a5
SeongMun Woo <tjdans7665@gmail.com>
2024-09-19 23:25:28 +0900

    9/19 - solve(3)

--- baekjoon/src/브루트포스/Baek_23301.java
+++ baekjoon/src/브루트포스/Baek_23301.java
@@ -1,20 +1,40 @@
 package 브루트포스;
 
-import java.util.*;
 import java.io.*;
 
 public class Baek_23301 {
-	static int n,t,k;
-	static int[][] times;
-	public static void main(String[] args) throws Exception{
+	static int n, t, k, answer=0,max=0;
+	static int[] times = new int[1001];
+	static int[] subSum = new int[1001];
+	
+	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("src/input/Baek_23301.txt"));
 		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-		
+
 		String[] line = br.readLine().trim().split(" ");
 		n = Integer.parseInt(line[0]);
-		t = Integer.parseInt(line[0]);
-		for(int p=0;p<n;p++) {
-			 k = Integer.parseInt(br.readLine());
+		t = Integer.parseInt(line[1]);
+
+		for (int p = 0; p < n; p++) {
+			k = Integer.parseInt(br.readLine());
+			for (int i = 0; i < k; i++) {
+				line = br.readLine().trim().split(" ");
+				int start = Integer.parseInt(line[0]);
+				int end = Integer.parseInt(line[1]);
+				for(int time=start+1;time<=end;time++) times[time]++;
+			}
 		}
+		
+		for(int i = 1;i<1001;i++) {
+			subSum[i] = subSum[i-1]+times[i];
+		}
+		
+		for(int i = 0;i<1001-t;i++) {
+			if(max<subSum[i+t]-subSum[i]) {
+				max = subSum[i+t]-subSum[i];
+				answer = i;
+			}
+		}
+		System.out.println(answer+" "+(answer+t));
 	}
 }
