e9a7c65b3adda5bd09c92fba4d688ecb7a2f020b
unknown <tjdans7665@gmail.com>
2025-01-24 00:42:08 +0900

    250123 solve problem/16924

--- src/bellman_ford/Baek_16924.java
+++ src/bellman_ford/Baek_16924.java
@@ -3,6 +3,7 @@ package bellman_ford;
 import java.io.BufferedReader;
 import java.io.FileInputStream;
 import java.io.InputStreamReader;
+import java.util.ArrayList;
 import java.util.StringTokenizer;
 
 public class Baek_16924 {
@@ -10,11 +11,12 @@ public class Baek_16924 {
     static BufferedReader br;
     static StringBuilder sb = new StringBuilder();
 
-    static String line;
-    static int n, m,cnt=0;
+    static int n, m;
     static char[][] map;
+    static boolean[][] visited;
     static int[] dx = {-1, 0, 1, 0};
     static int[] dy = {0, 1, 0, -1};
+    static ArrayList<int[]> result = new ArrayList<>();
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("src/input/Baek_16924.txt"));
@@ -23,40 +25,77 @@ public class Baek_16924 {
         n = Integer.parseInt(st.nextToken());
         m = Integer.parseInt(st.nextToken());
         map = new char[n][m];
+        visited = new boolean[n][m];
+
         for (int i = 0; i < n; i++) {
-            line = br.readLine();
+            String line = br.readLine();
             for (int j = 0; j < m; j++) {
-                map[i][j] = line.charAt(j);;
+                map[i][j] = line.charAt(j);
             }
         }
 
         for (int i = 0; i < n; i++) {
             for (int j = 0; j < m; j++) {
-                if(map[i][j] == '*') check(i, j);
+                if (map[i][j] == '*') {
+                    int maxLen = getMaxLen(i, j);
+                    if (maxLen > 0) {
+                        visited[i][j] = true;
+                        for (int len = 1; len <= maxLen; len++) {
+                            for (int d = 0; d < 4; d++) {
+                                int nx = i + dx[d] * len;
+                                int ny = j + dy[d] * len;
+                                visited[nx][ny] = true;
+                            }
+                        }
+                        result.add(new int[]{i + 1, j + 1, maxLen});
+                    }
+                }
             }
         }
-        System.out.println(cnt==0?-1:cnt);
-        if(cnt!=0) System.out.println(sb);
+
+        if (!isValid()) {
+            System.out.println(-1);
+        } else {
+            System.out.println(result.size());
+            for (int[] cross : result) {
+                sb.append(cross[0]).append(" ")
+                        .append(cross[1]).append(" ")
+                        .append(cross[2]).append("\n");
+            }
+            System.out.print(sb);
+        }
     }
 
-    static void check(int x, int y) {
-        boolean flag = false;
-        for(int len = 1; len <= 3; len++) {
-            for(int i = 0;i<4;i++){
-                int row = x+dx[i]*len;
-                int col = y+dy[i]*len;
-                if(!isBound(row,col)||map[row][col] != '*'){
-                    flag = true;
+    static int getMaxLen(int x, int y) {
+        int size = 0;
+        while (true) {
+            boolean flag = true;
+            for (int i = 0; i < 4; i++) {
+                int nx = x + dx[i] * (size + 1);
+                int ny = y + dy[i] * (size + 1);
+                if (!isBound(nx, ny) || map[nx][ny] != '*') {
+                    flag = false;
                     break;
                 }
             }
-            if(flag) break;
-            cnt++;
-            sb.append(x+1).append(" ").append(y+1).append(" ").append(len).append("\n");
+            if (!flag) break;
+            size++;
         }
+        return size;
     }
 
     static boolean isBound(int x, int y) {
         return !(x < 0 || x >= n || y < 0 || y >= m);
+    }
+
+    static boolean isValid() {
+        for (int i = 0; i < n; i++) {
+            for (int j = 0; j < m; j++) {
+                if (map[i][j] == '*' && !visited[i][j]) {
+                    return false;
+                }
+            }
+        }
+        return true;
     }
 }
