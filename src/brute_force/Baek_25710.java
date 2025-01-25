ae0cdfc29ba2e4840391746d9f2da5096f6985a5
unknown <tjdans7665@gmail.com>
2025-01-25 17:51:41 +0900

    250125 solve problem/25710

--- src/brute_force/Baek_25710.java
+++ src/brute_force/Baek_25710.java
@@ -1,36 +1,47 @@
 package brute_force;
 
 import java.io.*;
+import java.util.HashSet;
+import java.util.StringTokenizer;
 
 public class Baek_25710 {
-	static int n, max = 0,cnt=0;
-	static int[] a = new int[1000];
-	static boolean[] check = new boolean[1000];
+	static StringTokenizer st;
+	static BufferedReader br;
+
+	static int n, max = 0;
+	static HashSet<Integer> set = new HashSet<>();
+	static int[] arr;
 
 	public static void main(String[] args) throws Exception {
 		System.setIn(new FileInputStream("src/input/Baek_25710.txt"));
-		BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+		br = new BufferedReader(new InputStreamReader(System.in));
 
-		n = Integer.parseInt(br.readLine().trim());
-		String[] line = br.readLine().trim().split(" ");
+		n = Integer.parseInt(br.readLine());
+		st = new StringTokenizer(br.readLine());
 		for (int i = 0; i < n; i++) {
-			int num = Integer.parseInt(line[i]);
-			if (check[num]) continue;
-			a[cnt++] = num;
-			check[num] = true;
+			int num = Integer.parseInt(st.nextToken());
+			if(set.contains(num)){
+				check((num*num)+"");
+				continue;
+			}
+			set.add(num);
 		}
-
-		for (int i = 0; i < cnt; i++) for (int j = i; j < cnt; j++) checkMax(a[i] * a[j]);
-
+		arr = new int[set.size()];
+		int idx = 0;
+		for(Integer i : set) arr[idx++] = i;
+		for(int i = 0;i<set.size()-1;i++){
+			for(int j = i+1;j<set.size();j++){
+				check((arr[i]*arr[j])+"");
+			}
+		}
 		System.out.println(max);
 	}
 
-	static void checkMax(int num) {
+	static void check(String num){
 		int sum = 0;
-		while (num > 0) {
-			sum += num % 10;
-			num /= 10;
+		for(int i=0;i<num.length();i++){
+			sum += num.charAt(i)-'0';
 		}
-		max = Math.max(max, sum);
+		max = Math.max(max,sum);
 	}
 }
