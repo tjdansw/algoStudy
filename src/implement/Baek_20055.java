3b18297b2b1100fb282cf060c5f761b4e2adf7bc
우성문 <tjdans7665@gmail.com>
2025-03-07 00:22:51 +0900

    250306 solve problem/20055

--- src/implement/Baek_20055.java
+++ src/implement/Baek_20055.java
@@ -1,32 +1,17 @@
 package implement;
 
-import java.io.BufferedReader;
-import java.io.FileInputStream;
-import java.io.InputStreamReader;
-import java.util.StringTokenizer;
+import java.io.*;
+import java.util.*;
 
 public class Baek_20055 {
     static StringTokenizer st;
     static BufferedReader br;
 
-    static int n, k, up, plus, step=0;
-    static int[] down = new int[2];
+    static int n, k, start, step=0;
     static int[] belt;
-    static Robot head, tail;
+    static boolean[] robot;
 
-    static class Robot{
-        int position;
-        Robot front,back;
 
-        public Robot(int position) {
-            this.position = position;
-        }
-
-        public Robot(int position, Robot front) {
-            this.position = position;
-            this.front = front;
-        }
-    }
 
     public static void main(String[] args) throws Exception {
         System.setIn(new FileInputStream("src/input/Baek_20055.txt"));
@@ -34,45 +19,45 @@ public class Baek_20055 {
         st = new StringTokenizer(br.readLine());
         n = Integer.parseInt(st.nextToken());
         k = Integer.parseInt(st.nextToken());
-        belt = new int[2*n];
-        plus = 2*n-1;
-
+        belt = new int[2 * n];
         st = new StringTokenizer(br.readLine());
-        for (int i = 0; i < 2*n; i++) belt[i] = Integer.parseInt(st.nextToken());
+        for (int i = 0; i < 2 * n; i++) belt[i] = Integer.parseInt(st.nextToken());
 
-        while (k>0) {
-            up = plus%(2*n);
-            down[0] = (up+n-1)%(2*n);
-            down[1] = (up+n)%(2*n);
-            Robot now = head;
+        robot = new boolean[n];
+        start = 0;
 
-            while(now != null) {
-                int nextPosition = (now.position+1)%(2*n);
-                if(now == head && (now.position==down[0]||now.position==down[1])) {
-                    head = now.back;
-                }
-                if((now.front==null||now.front.position!=nextPosition)&&belt[nextPosition] != 0){
-                    belt[nextPosition]--;
-                    if(belt[nextPosition]==0)k--;
-                    now.position = nextPosition;
-                }
-                now = now.back;
-            }
-
-            if(belt[up]!=0){
-                if(head == null){
-                    head = new Robot(up);
-                    tail = head;
-                }else{
-                    tail.back = new Robot(up,tail);
-                    tail = tail.back;
-
-                }
-                belt[up]--;
-                if(belt[up]==0) k--;
-            }
+        while(k>0) {
             step++;
-            plus = plus==0?2*n-1:plus-1;
+
+            start = (start - 1 + 2 * n) % (2 * n);
+
+            for (int i = n - 1; i > 0; i--) {
+                robot[i] = robot[i - 1];
+            }
+            robot[0] = false;
+            if (robot[n - 1]) {
+                robot[n - 1] = false;
+            }
+
+            for (int i = n - 2; i >= 0; i--) {
+                int nextPosition = (start + i + 1) % (2 * n);
+                if (robot[i] && !robot[i + 1] && belt[nextPosition] > 0) {
+                    robot[i] = false;
+                    robot[i + 1] = true;
+                    belt[nextPosition]--;
+                    if (belt[nextPosition] == 0) k--;
+                }
+            }
+
+            if (robot[n - 1]) {
+                robot[n - 1] = false;
+            }
+
+            if (belt[start] > 0) {
+                robot[0] = true;
+                belt[start]--;
+                if (belt[start] == 0) k--;
+            }
         }
 
         System.out.println(step);
