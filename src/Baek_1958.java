60658b82ebe7b206e1272642b07e5ccd1c4d5705
우성문 <tjdans7665@gmail.com>
2025-08-06 22:28:13 +0900

    250806 solve problem

---
+++ src/Baek_1958.java
@@ -0,0 +1,25 @@
+import java.io.*;
+
+// 1958
+public class Baek_1958 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        char[][] wordArray = {br.readLine().toCharArray(),br.readLine().toCharArray(),br.readLine().toCharArray()};
+        int n = wordArray[0].length;
+        int p = wordArray[1].length;
+        int q = wordArray[2].length;
+        int[][][] dp = new int[n+1][p+1][q+1];
+        for(int i = 1;i<=n;i++){
+            for(int j = 1;j<=p;j++){
+                for (int k = 1;k<=q;k++){
+                    dp[i][j][k] = Math.max(Math.max(dp[i-1][j][k],dp[i][j-1][k]),dp[i][j][k-1]);
+                    if(wordArray[0][i-1]==wordArray[1][j-1]&&wordArray[1][j-1]==wordArray[2][k-1]){
+                        dp[i][j][k] = Math.max(dp[i][j][k],dp[i-1][j-1][k-1]+1);
+                    }
+                }
+            }
+        }
+        System.out.println(dp[n][p][q]);
+    }
+}
