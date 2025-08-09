4c3cfcb797842ebf124d09f88968cbe391afa60d
우성문 <tjdans7665@gmail.com>
2025-08-09 19:32:10 +0900

    250809 solve problem

--- src/implement/Baek_11723.java
+++ src/implement/Baek_11723.java
@@ -1,86 +1,46 @@
 package implement;
 
 import java.io.BufferedReader;
-import java.io.FileInputStream;
 import java.io.InputStreamReader;
 import java.util.StringTokenizer;
 
 public class Baek_11723 {
-    static StringTokenizer st;
-    static BufferedReader br;
-    static StringBuilder sb = new StringBuilder();
-
-    static int m;
-    static int bitMask = 0;
-
     public static void main(String[] args) throws Exception {
-        System.setIn(new FileInputStream("src/input/Baek_11723.txt"));
-        br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringBuilder sb = new StringBuilder();
 
-        m = Integer.parseInt(br.readLine());
+        int m = Integer.parseInt(br.readLine());
+        int S = 0;
 
         for (int i = 0; i < m; i++) {
-            st = new StringTokenizer(br.readLine());
-            switch (st.nextToken()) {
-                case "add":{
-                    int num = Integer.parseInt(st.nextToken());
-                    addNum(num);
-                    break;
-                }
-                case "remove":{
-                    int num = Integer.parseInt(st.nextToken());
-                    removeNum(num);
-                    break;
-                }
-                case "check":{
-                    int num = Integer.parseInt(st.nextToken());
-                    sb.append(containsNum(num)).append('\n');
-                    break;
-                }
-                case "toggle":{
-                    int num = Integer.parseInt(st.nextToken());
-                    toggleNum(num);
-                    break;
-                }
-                case "all":{
-                    all();
-                    break;
-                }
-                case "empty":{
-                    empty();
-                    break;
+            String line = br.readLine();
+            if (line.equals("all")) {
+                S = (1 << 21) - 2;
+            } else if (line.equals("empty")) {
+                S = 0;
+            } else {
+                StringTokenizer st = new StringTokenizer(line);
+                String cmd = st.nextToken();
+                int x = Integer.parseInt(st.nextToken());
+                int bit = 1 << x;
+
+                switch (cmd) {
+                    case "add":
+                        S |= bit;
+                        break;
+                    case "remove":
+                        S &= ~bit;
+                        break;
+                    case "check":
+                        sb.append(((S & bit) != 0) ? 1 : 0).append('\n');
+                        break;
+                    case "toggle":
+                        S ^= bit;
+                        break;
                 }
             }
         }
 
-        System.out.println(sb);
-    }
-
-    public static void addNum(int num) {
-        bitMask |= (1 << num);
-    }
-
-    public static void removeNum(int num) {
-        bitMask &= ~(1 << num);
-    }
-
-    public static int containsNum(int num) {
-        return (bitMask & (1 << num)) != 0?1:0; // 해당 숫자 위치의 비트가 1인지 확인
-    }
-
-    public static void toggleNum(int num) {
-        if (containsNum(num) == 1) {
-            removeNum(num);
-        } else {
-            addNum(num);
-        }
-    }
-
-    public static void all(){
-        bitMask = (1 << 22) - 1;
-    }
-
-    public static void empty(){
-        bitMask = 0;
+        System.out.print(sb.toString());
     }
 }
