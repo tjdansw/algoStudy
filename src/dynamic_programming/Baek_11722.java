5b010d757eb1e8200542f23b59d337a1a0e4a756
우성문 <tjdans7665@gmail.com>
2025-08-22 21:48:44 +0900

    250822 solve problem

---
+++ src/dynamic_programming/Baek_11722.java
@@ -0,0 +1,31 @@
+package dynamic_programming;
+
+import java.util.*;
+import java.io.*;
+
+// 11722
+public class Baek_11722 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        int n = Integer.parseInt(br.readLine());
+        int[] nums = new int[n];
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        for (int i = 0; i < n; i++) {
+            nums[i] = Integer.parseInt(st.nextToken());
+        }
+
+        int[] dp = new int[n];
+        Arrays.fill(dp, 1);
+        int max = 1;
+        for (int i = 1; i < n; i++) {
+            for (int j = 0; j < i; j++) {
+                if (nums[j] > nums[i]) {
+                    dp[i] = Math.max(dp[i], dp[j] + 1);
+                }
+            }
+            max = Math.max(max, dp[i]);
+        }
+        System.out.println(max);
+    }
+}
