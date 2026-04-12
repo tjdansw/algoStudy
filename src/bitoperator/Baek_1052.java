69268102520941f0ca02a07c3070a1694cf6936d
우성문 <tjdans7665@gmail.com>
2026-04-12 15:46:13 +0900

    260412 solve problem

---
+++ src/bitoperator/Baek_1052.java
@@ -0,0 +1,31 @@
+package bitoperator;
+
+import java.util.*;
+import java.io.*;
+
+// 1052
+public class Baek_1052 {
+
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
+
+        System.out.println(Integer.bitCount(n));
+        if (k >= Integer.bitCount(n)) {
+            System.out.println(0);
+            return;
+        }
+
+        int answer = 0;
+
+        while (Integer.bitCount(n) > k) {
+            n++;
+            answer++;
+        }
+
+        System.out.println(answer);
+    }
+}
